460f353b78d81d77964bc7d3c17864dd4c9276b6
NaimeJuliet <naime2008@hotmail.com>
2022-01-25 10:02:00 -0500

    se crea tienda y controladores para la tienda

---
+++ js/controlador.js
@@ -0,0 +1,6 @@
+///importacion  de elemento
+import {llenartienda} from "./llenartienda.js"
+
+
+//llamo a la funcion llamar tienda
+llenartienda()
---
+++ js/llenartienda.js
@@ -0,0 +1,24 @@
+//funcion para llenar dinamicamente la tienda
+
+function llenartienda(){
+    export function llenartienda(){
+
+    let productos=[
+
+        {nombre:"cartuchera", precio: 50000, foto:"../img/cartuchera.jpg" , descripcion:"cartuchera para guardar tus objetos personales"}
+        {nombre:"camara", precio: 150000, foto:"../img/camara.jpg" , descripcion:"hermosa camara con modelo de mickey antiguo"}
+        {nombre:"camara minnie", precio: 150000, foto:"../img/camara2.jpg" , descripcion:"hermosa y comoda camara de minnie mouse"}
+        {nombre:"collar minnie", precio: 90000, foto:"../img/collar_minnie.jpg" , descripcion:"collar minie para todas las edades"}
+        {nombre:"cortauñas", precio: 25000, foto:"../img/cortauñas.jpg" , descripcion:"un cortauñas hermoso, delicado para todas las edades"}
+        {nombre:"morral", precio: 120000, foto:"../img/morral.jpg" , descripcion:"comodo morral para llevar todo lo que quieras"}
+        {nombre:"muñeco", precio: 75000, foto:"../img/muñeco.jpg" , descripcion:"peluche para los mas peques"}
+        {nombre:"taza", precio: 50000, foto:"../img/taza.jpg" , descripcion:"preciosa taza para beber tus mejores momentos"}
+        {nombre:"vaso", precio: 12000, foto:"../img/vaso.jpg" , descripcion:"comoda y hermosa taza con calentador para que tus bebidas siempre esten calientes"}
+        {nombre:"cosmetiquera", precio: 65000, foto:"../img/cosmetiquera.jpg" , descripcion:"guarda todos tus comesticos en una hermosa cosmetiquera"}
+    ]
+  
+
+    }
+console.log(productos)
+
+}
