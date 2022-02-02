dbf09b99e5375ff98b40a1dd746df8bfa53a97b9
NaimeJuliet <naime2008@hotmail.com>
2022-02-02 18:23:44 -0500

    se creo documento agregarAlCarrito

---
+++ js/agregarAlCarrito.js
@@ -0,0 +1,14 @@
+export function agregarCarrito(carrito){
+    //crear una referencia a la pildora (push) del carrito
+    let pildora=document.getElementById("pildora")
+    let cantidad=0
+
+    carrito.forEach(function(producto){
+        cantidad=cantidad+producto.cantidad
+    })
+
+    pildora.textContent=cantidad
+    pildora.classList.remove("invisible")
+    console.log(carrito)
+
+}
--- js/controlador.js
+++ js/controlador.js
@@ -1,9 +1,11 @@
 ///importacion  de elemento
 import {llenartienda} from "./llenartienda.js"
 import {AmpliarInfoProducto}from "./ampliarInfoProducto.js"
+import {agregarCarrito}from "./agregarAlCarrito.js"
 
 //variables globales de la tienda
 let producto={}
+let carrito=[]
 
 //llamo a la funcion llenar tienda
 llenartienda()
@@ -16,7 +18,30 @@ let listaProductos=document.getElementById("fila")
 listaProductos.addEventListener("click", function(event){
 
         producto=AmpliarInfoProducto(event)
-        console.log(producto)
         modalinfoproducto.show()
+
+    })
+
+    //llamo la funcion agregar al carrito
+    let botonAGregarCarrito=document.getElementById("botonAgregarProducto")
+    botonAGregarCarrito.addEventListener("click",function(){
+
+        let cantidadProducto=document.getElementById("cantidadProducto").value
+        producto.cantidad=cantidadProducto
+        
+
+         //agrego el producto al carrito
+        carrito.push(producto)
+        
+
+        //Oculto la modal de info producto
+        modalinfoproducto.hide()  
+
+        //llamar la funcion agregarCarrito
+        agregarCarrito(carrito)
+
+
+
+        
 
     })
